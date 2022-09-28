5df8a920525677cb44df1e84dd6a957d632c636b
diana <diana.garcia.sanchez@utelvt.edu.ecgit config --global -egit config --global user.email diana.garcia.sanchez@utelvt.edu.ec>
2022-09-27 20:31:14 -0500

    programa de compara

---
+++ compara.cpp
@@ -0,0 +1,20 @@
+#include<iostream> 
+ using namespace std; 
+ int main() 
+ { 
+         float d,s; 
+         cout<<"ingrese d s:"; cin>>d>>s; 
+         if(d>s) 
+         { 
+           cout<<d<<" es mayor d "<<s<<endl; 
+  
+  
+         }else{ 
+  
+           cout<<s<<" es mayor d "<<d<<endl; 
+  
+  
+         } 
+  
+         return 0;
+ }
