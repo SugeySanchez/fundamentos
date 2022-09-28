bf1d95af9c0073fe5f7cd28ae12677a4b4efbfcf
diana <diana.garcia.sanchez@utelvt.edu.ecgit config --global -egit config --global user.email diana.garcia.sanchez@utelvt.edu.ec>
2022-09-27 19:49:24 -0500

    programa de velocimetro.cpp

---
+++ velocimetro.cpp
@@ -0,0 +1,19 @@
+
+  
+  
+  
+#include<iostream> 
+ using namespace std; 
+  
+ int main() 
+ { 
+         float d,s,g; 
+         cout<<"Ingrese la distancia(en km) d= "; cin>>d; 
+         cout<<"Ingrese el tiempo(en horas)  s= "; cin>>s; 
+         g=d/s; 
+         cout<<"La velocidad calculada es de g= "<<g<<endl<<endl; 
+         return 0; 
+  
+  
+  
+ }
